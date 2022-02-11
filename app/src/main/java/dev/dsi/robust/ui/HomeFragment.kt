225e41092fbe31acdb6fc2520a3de10eafb9519b
Debayan Ghosh Dastider <debz.exe@gmail.com>
2022-02-12 03:25:49 +0530

    Added Nav Graph and basic fragments

---
+++ app/src/main/java/dev/dsi/robust/ui/HomeFragment.kt
@@ -0,0 +1,28 @@
+package dev.dsi.robust.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.dsi.robust.R
+
+
+class HomeFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+
+}
