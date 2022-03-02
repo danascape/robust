412875a3ec732abf951d226f3fbe5e08dfe6c64d
Madhur Mehta <12madhurmehta@gmail.com>
2022-03-02 15:41:04 +0530

    Added splash screen and solved the expiry bug

---
+++ app/src/main/java/dev/dsi/robust/SplashActivity.kt
@@ -0,0 +1,23 @@
+package dev.dsi.robust
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import dev.dsi.robust.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1000)
+        setContentView(view)
+    }
+}
