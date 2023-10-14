752893ccd94bc78b2c9f131ccbbdcd1c50e26f61
Rajit Deb <rajitdeb456@gmail.com>
2023-10-14 10:35:42 +0530

    Fixed issue (#3) - findViewById used instead of binding in MainActivity. Also did some code sanitization and cleanup

--- app/src/main/java/dev/dsi/robust/MainActivity.kt
+++ app/src/main/java/dev/dsi/robust/MainActivity.kt
@@ -3,21 +3,27 @@ package dev.dsi.robust
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import dev.dsi.robust.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-        val navHost = findViewById<View>(R.id.navHostFragment)
-        binding.bottomNav.setupWithNavController(navHost.findNavController())
+        setContentView(binding.root)
+
+        val navHost = supportFragmentManager
+            .findFragmentById(binding.navHostFragment.id) as NavHostFragment
+
+        val navController: NavController = navHost.navController
+
+        binding.bottomNav.setupWithNavController(navController)
 
     }
 }
